a5c2319627feb3e77fdb4c6b76765fa7c0971759
shubham-rp <shubhamphadte37@gmail.com>
2023-10-17 17:04:15 +0530

    Simple Dashboard UI with material ui icon

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { makeStyles } from "tss-react/mui";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import OfflineBoltIcon from "@mui/icons-material/OfflineBolt";
+
+const useStyles = makeStyles()(() => {
+  return {
+    box: {
+      height: "100vh",
+      backgroundColor: "#667bf2",
+      backgroundImage: `url(
+        "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1600 900'%3E%3Cpolygon fill='%23f0b608' points='957 450 539 900 1396 900'/%3E%3Cpolygon fill='%23e6d710' points='957 450 872.9 900 1396 900'/%3E%3Cpolygon fill='%23e7af05' points='-60 900 398 662 816 900'/%3E%3Cpolygon fill='%23e7d808' points='337 900 398 662 816 900'/%3E%3Cpolygon fill='%23d8a408' points='1203 546 1552 900 876 900'/%3E%3Cpolygon fill='%23f1e213' points='1203 546 1552 900 1162 900'/%3E%3Cpolygon fill='%23f0b607' points='641 695 886 900 367 900'/%3E%3Cpolygon fill='%23e4d506' points='587 900 641 695 886 900'/%3E%3Cpolygon fill='%23eab822' points='1710 900 1401 632 1096 900'/%3E%3Cpolygon fill='%23e8da14' points='1710 900 1401 632 1365 900'/%3E%3Cpolygon fill='%23e8b008' points='1210 900 971 687 725 900'/%3E%3Cpolygon fill='%23edde14' points='943 900 1210 900 971 687'/%3E%3C/svg%3E"
+      )`,
+      backgroundAttachment: "fixed",
+      backgroundSize: "cover",
+    },
+  };
+});
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const { classes } = useStyles();
+  return (
+    <>
+      <Box className={classes.box}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <OfflineBoltIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Dashboard
+            </Typography>
+            <Button color="inherit">LOGOUT</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 }
 
 export default Dashboard;
