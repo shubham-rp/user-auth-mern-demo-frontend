fc81918c6bbc5d232d7673dbefe933e4c72d55ac
shubham-rp <shubhamphadte37@gmail.com>
2023-10-17 12:58:57 +0530

    Login Page UI with Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hi</div>;
+    return (
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </div>
+    );
   }
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,96 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Button,
+  Divider,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()((theme) => {
+  return {
+    loginContainer: {
+      backgroundColor: "#cfe8fc",
+      height: "100vh",
+    },
+    signUpLink: {
+      height: "5vh",
+      textAlign: "right",
+    },
+    signUpButton: {
+      marginTop: "8px",
+      width: "128px",
+      backgroundColor: "teal",
+      textDecoration: "none",
+      color: "white",
+    },
+    loginBox: {
+      height: "95vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    loginHeading: {
+      fontSize: "32px",
+      color: "teal",
+    },
+    loginTextField: {
+      width: "512px",
+      backgroundColor: "white",
+    },
+    loginButton: {
+      width: "128px",
+      backgroundColor: "teal",
+    },
+  };
+});
 
 function Login() {
-  return <div>Login</div>;
+  const { classes } = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="lg" className={classes.loginContainer}>
+        <Box className={classes.signUpLink}>
+          <Link to="/signup">
+            <Button variant="contained" className={classes.signUpButton}>
+              Sign Up
+            </Button>
+          </Link>
+        </Box>
+        <Box className={classes.loginBox}>
+          <Stack spacing={4} alignItems="center">
+            <Typography className={classes.loginHeading}>
+              {" "}
+              USER AUTH DEMO LOGIN{" "}
+            </Typography>
+            <Divider flexItem />
+            <TextField
+              className={classes.loginTextField}
+              variant="outlined"
+              label="Email"
+              helperText="Some important text"
+            />
+            <TextField
+              className={classes.loginTextField}
+              variant="outlined"
+              label="Password"
+              type="password"
+              helperText="Some important text"
+            />
+            <Button variant="contained" className={classes.loginButton}>
+              Sign In
+            </Button>
+          </Stack>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 export default Login;
